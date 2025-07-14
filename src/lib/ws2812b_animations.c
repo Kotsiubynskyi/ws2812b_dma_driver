faa985fd08cac8c53be8b256b947937a2af246c7
Eugene <turboopera@gmail.com>
2025-07-14 21:34:58 +0300

    Update

--- src/lib/ws2812b_animations.c
+++ src/lib/ws2812b_animations.c
@@ -1,28 +1,41 @@
+#include "stm32g0xx_hal.h"
 #include "ws2812b.h"
-
 #include "ws2812b_animations.h"
 
 static uint32_t wheel(uint8_t pos) {
+    uint8_t red, green, blue;
     pos = 255 - pos;
+    
     if (pos < 85) {
-        return ((uint32_t)(255 - pos * 3) << 16) | ((uint32_t)(0) << 8) | (pos * 3);
-    }
-    if (pos < 170) {
+        red = 255 - pos * 3;
+        green = 0;
+        blue = pos * 3;
+    } 
+    else if (pos < 170) {
         pos -= 85;
-        return ((uint32_t)(0) << 16) | ((uint32_t)(pos * 3) << 8) | (255 - pos * 3);
+        red = 0;
+        green = pos * 3;
+        blue = 255 - pos * 3;
     }
-    pos -= 170;
-    return ((uint32_t)(pos * 3) << 16) | ((uint32_t)(255 - pos * 3) << 8) | (0);
+    else {
+        pos -= 170;
+        red = pos * 3;
+        green = 255 - pos * 3;
+        blue = 0;
+    }
+    
+    return (uint32_t)red << 16 | (uint32_t)green << 8 | blue;
 }
 
 void rainbow(void) {
-    uint8_t j = 0;
+    uint8_t position = 0;
+    
     while (1) {
-        for (uint32_t i = 0; i < PIXELS_AMOUNT; i++) {
-            ws2812b_set_pixel(i, wheel(((i * 256 / PIXELS_AMOUNT) + j) & 255));
+        for (uint32_t pixel = 0; pixel < PIXELS_AMOUNT; pixel++) {
+            uint8_t color_index = ((pixel * 256 / PIXELS_AMOUNT) + position) & 255;
+            ws2812b_set_pixel(pixel, wheel(color_index));
         }
-        HAL_Delay(20);
-        j++;
-        if (j >= 256) j = 0;
+        HAL_Delay(15);
+        position = (position + 1) & 255;
     }
 }
